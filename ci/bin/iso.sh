75e136f4ee323fa524b2666859d2ee48eb4a0425
Yael Adler <yael.adler@integnology.com>
2019-04-17 15:41:10 -0700

    [GX-3260] Fix isolation script

--- ci/bin/iso.sh
+++ ci/bin/iso.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cp build.properties.iso build.properties
+export PATH=${PATH}:${PKG_BASE_DIR}/ant/apache-ant-${ANT_VERSION}/bin
+export JAVA_HOME=${PKG_BASE_DIR}/jdk/jdk-12
+export PATH=$PATH:$JAVA_HOME/bin
+
+cp -f ${PKG_BASE_DIR}/${APPLICATION_NAME}/sample/build.properties.iso build.properties
 ant test
