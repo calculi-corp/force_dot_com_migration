5c04007d548dc27e47181cfe659450a19bbeadd0
Yael Adler <yael.adler@integnology.com>
2019-04-17 11:18:48 -0700

    [GX-3260] Add java home to start script

--- ci/bin/start-service.sh
+++ ci/bin/start-service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH=${PATH}:${PKG_BASE_DIR}/ant/apache-ant-${ANT_VERSION}/bin
-
+export JAVA_HOME=/var/vcap/packages/jdk/jdk1.8.0_181
+export PATH=$PATH:$JAVA_HOME/bin
 echo "checking for ant version"
 while true
 do
