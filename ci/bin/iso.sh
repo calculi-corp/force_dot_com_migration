32eb9f8fc92eb12c396f7c63f70fe7e24e2fbc63
Yael Adler <yael.adler@integnology.com>
2019-04-17 16:07:48 -0700

    [GX-3260] Fix iso.sh file

--- ci/bin/iso.sh
+++ ci/bin/iso.sh
@@ -3,5 +3,6 @@ export PATH=${PATH}:${PKG_BASE_DIR}/ant/apache-ant-${ANT_VERSION}/bin
 export JAVA_HOME=${PKG_BASE_DIR}/jdk/jdk-12
 export PATH=$PATH:$JAVA_HOME/bin
 
-cp -f ${PKG_BASE_DIR}/${APPLICATION_NAME}/sample/build.properties.iso build.properties
+pushd ${PKG_BASE_DIR}/force_dot_com_migration/sample
+cp build.properties.iso build.properties
 ant test
