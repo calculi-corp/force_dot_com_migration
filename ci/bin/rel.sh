78ba235de47fa9d81e19caa2e2103dbb54cb38f5
Yael Adler <yael.adler@integnology.com>
2019-04-18 10:16:36 -0700

    [GX-3261] Update rel.sh

--- ci/bin/rel.sh
+++ ci/bin/rel.sh
@@ -1,3 +1,8 @@
-#! /usr/bin/bash
+#!/bin/bash
+export PATH=${PATH}:${PKG_BASE_DIR}/ant/apache-ant-${ANT_VERSION}/bin
+export JAVA_HOME=${PKG_BASE_DIR}/jdk/jdk-12
+export PATH=$PATH:$JAVA_HOME/bin
+
+pushd ${PKG_BASE_DIR}/force_dot_com_migration/sample
 cp build.properties.rel build.properties
 ant test
